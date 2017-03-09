6dc2858f461f271d14edd3979f72a55194e36670
balazs4 <balazs4web@gmail.com>
2017-03-09 22:01:08 +0100

    parse/stringify functions

--- examples/helloasyncworld.js
+++ examples/helloasyncworld.js
@@ -1,16 +1,19 @@
 const log = require('debug')(require('path').parse(__filename).name);
 
+const sleep = ms => new Promise((resolve, reject) => {
+  setTimeout(
+    () => {
+      resolve();
+    },
+    ms
+  );
+});
+
 module.exports = ({ publish, subscribe, end }) => {
-  subscribe('/foo', async ({foo}) => {
+  subscribe('/foo', async ({ foo = 5 }) => {
     log('incoming message');
-    const outgoing = await new Promise((resolve, reject) => {
-      setTimeout(
-        () => {
-          resolve({ hello: foo * 2 });
-        },
-        foo * 100
-      );
-    });
+    await sleep(5000);
+    const outgoing = { foo: 'bar' };
     publish('/bar', outgoing);
     log('outgoing message...');
   });
--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const log = require('debug')('mano');
 const error = require('debug')('mano:err');
 const { connect } = require('mqtt');
 
+const parse = input => {
+  try {
+    return JSON.parse(input);
+  } catch (error) {
+    return input;
+  }
+};
+
+const stringify = msg => typeof msg === typeof {} ? JSON.stringify(msg) : msg;
+
 module.exports = (broker, onConnect, onClose) => {
   const client = connect(broker);
   client.on('offline', () => {
@@ -18,11 +28,11 @@ module.exports = (broker, onConnect, onClose) => {
   client.on('connect', () => {
     log(`connected to ${broker}...`);
     const publish = (topic, msg) => {
-      client.publish(topic, JSON.stringify(msg));
+      client.publish(topic, stringify(msg));
     };
     const subscribe = (topic, callback) => {
       client.on('message', (tpc, msg) => {
-        if (tpc === topic) callback(JSON.parse(msg.toString()));
+        if (tpc === topic) callback(parse(msg.toString()));
       });
       client.subscribe(topic);
     };
