5147181b3fcfb7f7d70dbdb00254724db4243215
balazs4 <balazs4web@gmail.com>
2017-03-18 14:43:05 +0100

    First 2 testcases

--- index.test.js
+++ index.test.js
@@ -1,22 +1,39 @@
 const { test } = require('ava');
 const mock = require('mock-require');
 
-// mock('mqtt', {
-//   connect: (url, config) => ({
-//     on: (event, handler) => {
-//       if (event === 'connect') handler();
-//       if (event === 'message') handler('/testtopic', 'hello');
-//     },
-//     subscribe: (topic, handler) => {
-//       handler();
-//     },
-//     publish: (topic, msg, config, handler) => {
-//       handler(topic, msg, (t, m) => {});
-//     }
-//   })
-// });
+mock('mqtt', {
+  connect: (url, config) => ({
+    on: (event, handler) => {
+      if (event === 'connect') handler();
+      if (event === 'message') handler('/test', 'foobar');
+    },
+    off: (event, handler) => {},
+    subscribe: (topic, config, handler) => {
+      handler();
+    },
+    publish: (topic, payload, config, handler) => {
+      handler();
+    }
+  })
+});
 
-// const mano = require('.');
+const mano = require('.');
+
+test('API', async t => {
+  const api = await mano('mqtt://fake.url', { clientId: 'test' });
+  t.is(typeof api.publish, typeof Function);
+  t.is(typeof api.subscribe, typeof Function);
+  t.is(typeof api.end, typeof Function);
+  t.is(api.clientId, 'test');
+});
+
+test('Happy path #subscribe', async t => {
+  const client = await mano('mqtt://fake.url', { clientId: 'test' });
+  const sub$ = await client.subscribe('/test');
+  sub$.onValue(x => {
+    t.deepEqual(x, { topic: '/test', payload: 'foobar' });
+  });
+});
 
 // test.cb('MQTT', t => {
 //   mano(
