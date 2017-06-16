c54339aa1076903c63f57f3c23381f33f946c1f0
balazs4 <balazs4web@gmail.com>
2017-06-16 13:09:34 +0200

    refactor bin file

--- bin/kifli.js
+++ bin/kifli.js
@@ -3,23 +3,32 @@ const { resolve, parse, join } = require('path');
 const args = require('args');
 const { name, main } = require('../package.json');
 
+args
+  .option('broker', 'MQTT Broker address', 'mqtt://localhost:1883')
+  .option('topic', 'MQTT topic with auto-subscription');
+
+const params = args.parse(process.argv);
+const file = args.sub[0];
+const kifli = require(join('..', main));
+
 const start = async (broker, topic, config, listener) => {
   const client = await kifli(broker, config);
   const topic$ = await client.subscribe(topic);
   topic$.onValue(listener(client));
 };
 
-args
-  .option('broker', 'MQTT Broker address', 'http://localhost:1883')
-  .option('topic', 'MQTT topic to auto-subscription');
-
-const params = args.parse(process.argv);
-const file = args.sub[0];
-const kifli = require(join('..', main));
+const createOptions = (o = {}) =>
+  Object.assign(
+    {},
+    {
+      clientId: [`${name}`, `${parse(file).name}`, `${process.pid}`].join('/')
+    },
+    o
+  );
 
 start(
   params['broker'],
   params['topic'],
-  { clientId: [`${name}`, `${parse(file).name}`, `${process.pid}`].join('/') },
+  createOptions(),
   require(resolve(process.cwd(), file))
 );
